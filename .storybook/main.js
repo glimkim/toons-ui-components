7f73051295051dae95e6eb8af51f21a1393ce4cb
glimkim <limkimdev@gmail.com>
2022-05-04 11:29:42 +0900

    feat: set up config files to build and publish library through npm

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
   framework: '@storybook/react',
   addons: [
     '@storybook/addon-links',
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Button from './stories/components/Button/Button';
+
+export { Button };
