c54e6d58375262733e6fd3ab28d6b34a84e762b4
glimkim <limkimdev@gmail.com>
2022-05-04 16:52:45 +0900

    refactor: improve ToonsThemeProvider
    so projects which use the package dont have to set their own theme

--- src/styles/ToonsThemeProvider.tsx
+++ src/styles/ToonsThemeProvider.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 // eslint-disable-next-line import/named
 import styled, { ThemeProvider, DefaultTheme } from 'styled-components';
+import DarkTheme from '../themes/dark';
+import Theme from '../themes/default';
 
+type ThemeType = 'DEFAULT' | 'DARK';
 interface ThemeProviderProps {
-  theme: DefaultTheme;
+  themeType: ThemeType;
   children: React.ReactNode;
 }
 
-function ToonsThemeProvider({ theme, children }: ThemeProviderProps) {
+function ToonsThemeProvider({ themeType, children }: ThemeProviderProps) {
+  const themes: {
+    [key in ThemeType]: DefaultTheme;
+  } = {
+    DARK: DarkTheme,
+    DEFAULT: Theme,
+  };
+  const theme: DefaultTheme = themes[themeType];
+
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
 
