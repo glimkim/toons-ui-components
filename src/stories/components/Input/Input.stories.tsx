a2ca48712d857a4e6b372de65673b9d8c30bb076
glimkim <limkimdev@gmail.com>
2022-05-25 14:37:37 +0900

    feat: develop input component veiw

--- src/stories/components/Input/Input.stories.tsx
+++ src/stories/components/Input/Input.stories.tsx
@@ -3,12 +3,45 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Input from './Input';
 
 export default {
-  title: 'Components/ListItem',
+  title: 'Components/Input',
   component: Input,
+  argTypes: {
+    fullWidth: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: true,
+    },
+    type: {
+      control: {
+        type: 'select',
+      },
+      defaultValue: 'text',
+    },
+    label: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'input label',
+    },
+    errorText: {
+      control: {
+        type: 'text',
+      },
+    },
+    placeholder: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'placeholder',
+    },
+  },
 } as ComponentMeta<typeof Input>;
 
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
 export const ToonsInput = Template.bind({});
 
-ToonsInput.args = {};
+ToonsInput.args = {
+  id: 'text-input',
+};
--- src/stories/components/Input/Input.tsx
+++ src/stories/components/Input/Input.tsx
@@ -2,24 +2,59 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface InputProps {
+  id: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   label?: string;
   placeholder?: string;
+  type?: 'email' | 'number' | 'password' | 'tel' | 'text' | 'time' | 'url';
   errorText?: string;
+  fullWidth?: boolean;
 }
 
-function Input({ onChange, label, placeholder, errorText }: InputProps) {
+function Input({ id, onChange, label, type = 'text', placeholder, errorText, fullWidth = true }: InputProps) {
   return (
-    <InputContainer>
-      {label && <label>{label}</label>}
+    <InputContainer fullWidth={fullWidth} hasError={!!errorText}>
+      {label && <label htmlFor={id}>{label}</label>}
       <div className="inputWrapper">
-        <input type="text" onChange={onChange} />
+        <input id={id} type={type} onChange={onChange} placeholder={placeholder} />
         {errorText && <span className="errorText">{errorText}</span>}
       </div>
     </InputContainer>
   );
 }
 
-const InputContainer = styled.div``;
+const InputContainer = styled.div<{ fullWidth: boolean; hasError: boolean }>`
+  width: ${({ fullWidth }) => (fullWidth ? '100%' : 'inherit')};
+  label {
+    display: block;
+    margin-bottom: 0.36rem;
+    font-size: 1rem;
+    color: ${({ theme }) => (theme.name === 'dark' ? theme.colors.gray00 : theme.colors.gray50)};
+  }
+
+  input {
+    width: 100%;
+    height: 2.67rem;
+    line-height: 2.67rem;
+    padding: 0 0.7rem;
+    border: none;
+    border-radius: 0.7rem;
+    box-shadow: 1px 2px 5px ${({ theme, hasError }) => (hasError ? theme.colors.red + '50' : '#00000015')};
+    background-color: ${({ theme }) => (theme.name === 'dark' ? theme.colors.gray00 : '#fff')};
+    color: ${({ theme }) => theme.colors.gray50};
+    transition: 0.3s ease-in-out;
+
+    &:focus {
+      box-shadow: 1px 2px 5px ${({ theme, hasError }) => (hasError ? theme.colors.red + '50' : theme.colors.main + 50)};
+    }
+  }
+
+  span.errorText {
+    display: block;
+    padding-top: 0.2rem;
+    color: ${({ theme: { colors } }) => colors.red};
+    font-size: 0.875rem;
+  }
+`;
 
 export default Input;
