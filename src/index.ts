be2d4bd5304f3359fd957fe22232037685234232
glimkim <limkimdev@gmail.com>
2022-05-25 14:37:48 +0900

    feat: export new components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { ToonsListItem } from './stories/components/ToonsListItem/ListItem.stories';
 import { Theme as ToonsTheme } from './themes/index';
 import theme from './themes/default';
 import darkTheme from './themes/dark';
 import Button from './stories/components/Button/Button';
+import Input from './stories/components/Input/Input';
+import ToonsListItem from './stories/components/ToonsListItem/ListItem';
+import Checkbox from './stories/components/Checkbox/Checkbox';
 import { ToonsGlobalCss } from './styles/globalStyle';
 
-export { Button, ToonsGlobalCss, ToonsListItem, theme, darkTheme };
+export { Button, Input, ToonsListItem, Checkbox, ToonsGlobalCss, theme, darkTheme };
 export type { ToonsTheme };
