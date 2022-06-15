6dfb224cbf123abc481daf80cd8a373ba7529d5e
glimkim <limkimdev@gmail.com>
2022-06-15 20:58:43 +0900

    feat: export Dialog component

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import Input from './stories/components/Input/Input';
 import ToonsListItem from './stories/components/ToonsListItem/ListItem';
 import Checkbox from './stories/components/Checkbox/Checkbox';
 import { ToonsGlobalCss } from './styles/globalStyle';
+import Dialog from './stories/components/Dialog/Dialog';
 
-export { Button, Input, ToonsListItem, Checkbox, ToonsGlobalCss, theme, darkTheme };
+export { Button, Input, ToonsListItem, Checkbox, Dialog, ToonsGlobalCss, theme, darkTheme };
 export type { ToonsTheme };
