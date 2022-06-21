80077fbd23a0e870ed31c85262f4ec5066c58811
glimkim <limkimdev@gmail.com>
2022-06-21 15:38:39 +0900

    style: component style update

--- src/stories/components/Loader/Loader.tsx
+++ src/stories/components/Loader/Loader.tsx
@@ -30,6 +30,14 @@ const ToonsLoader = styled.div<{ isPartial: boolean }>`
       ? css`
           width: 100%;
           height: 100%;
+          .lds-ellipsis {
+            width: 5.2rem;
+            height: 0.7rem;
+          }
+          .lds-ellipsis div {
+            width: 0.7rem;
+            height: 0.7rem;
+          }
         `
       : css`
           position: fixed;
@@ -37,69 +45,71 @@ const ToonsLoader = styled.div<{ isPartial: boolean }>`
           left: 0;
           width: 100vw;
           height: 100vh;
+          background-color: #00000070;
+          .lds-ellipsis {
+            width: 6rem;
+            height: 1rem;
+          }
+          .lds-ellipsis div {
+            width: 1rem;
+            height: 1rem;
+          }
         `;
   }}
 
-  background-color: #00000070;
-  ${({ theme }) => {
-    const mainColor = theme.colors.main;
-    return css`
-      .lds-ellipsis {
-        display: inline-block;
-        position: relative;
-      }
-      .lds-ellipsis div {
-        position: absolute;
-        width: 1rem;
-        height: 1rem;
-        border-radius: 50%;
-        background: #fff;
-        animation-timing-function: cubic-bezier(0, 1, 1, 0);
-        transition: 0.3s;
-      }
-      .lds-ellipsis div:nth-child(1) {
-        left: 8px;
-        animation: lds-ellipsis1 0.6s infinite;
-      }
-      .lds-ellipsis div:nth-child(2) {
-        left: 8px;
-        animation: lds-ellipsis2 0.6s infinite;
-        background: ${mainColor};
-      }
-      .lds-ellipsis div:nth-child(3) {
-        left: 32px;
-        animation: lds-ellipsis2 0.6s infinite;
-      }
-      .lds-ellipsis div:nth-child(4) {
-        left: 56px;
-        animation: lds-ellipsis3 0.6s infinite;
-      }
-      @keyframes lds-ellipsis1 {
-        0% {
-          transform: scale(0);
-        }
-        100% {
-          transform: scale(1);
-        }
-      }
-      @keyframes lds-ellipsis3 {
-        0% {
-          transform: scale(1);
-        }
-        100% {
-          transform: scale(0);
-        }
-      }
-      @keyframes lds-ellipsis2 {
-        0% {
-          transform: translate(0, 0);
-        }
-        100% {
-          transform: translate(24px, 0);
-        }
-      }
-    `;
-  }}
+  .lds-ellipsis {
+    display: inline-block;
+    position: relative;
+  }
+  .lds-ellipsis div {
+    position: absolute;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    background: #fff;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+    transition: 0.3s;
+  }
+  .lds-ellipsis div:nth-child(1) {
+    left: 8px;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(2) {
+    left: 8px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(3) {
+    left: 32px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(4) {
+    left: 56px;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(24px, 0);
+    }
+  }
 `;
 
 export default Loader;
