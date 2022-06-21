6145c63243828b7845585d18a7d3f2696e8bcaef
glimkim <limkimdev@gmail.com>
2022-06-21 15:27:50 +0900

    chore: export Loader and version upgrade

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import ToonsListItem from './stories/components/ToonsListItem/ListItem';
 import Checkbox from './stories/components/Checkbox/Checkbox';
 import { ToonsGlobalCss } from './styles/globalStyle';
 import Dialog from './stories/components/Dialog/Dialog';
+import Loader from './stories/components/Loader/Loader';
 
-export { Button, Input, ToonsListItem, Checkbox, Dialog, ToonsGlobalCss, theme, darkTheme };
+export { Button, Input, ToonsListItem, Checkbox, Loader, Dialog, ToonsGlobalCss, theme, darkTheme };
 export type { ToonsTheme };
