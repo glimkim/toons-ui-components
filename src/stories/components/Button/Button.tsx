6f4f42b32ca350116571e205603efae379374d7c
glimkim <limkimdev@gmail.com>
2022-07-28 20:58:40 +0900

    style: adjust hight and line height

--- src/stories/components/Button/Button.tsx
+++ src/stories/components/Button/Button.tsx
@@ -26,8 +26,8 @@ const ButtonComponent = styled.button<{ size: ButtonSize; fullWidth: boolean; bu
   display: block;
   padding: ${(props) => (props.size === 'medium' ? '0 1rem' : props.size === 'large' ? '0 1.43rem' : '0 0.7rem')};
   width: ${(props) => (props.fullWidth ? '100%' : 'fit-content')};
-  height: ${(props) => (props.size === 'medium' ? '3rem' : props.size === 'large' ? '3.14rem' : '2.57rem')};
-  line-height: ${(props) => (props.size === 'medium' ? '3rem' : props.size === 'large' ? '3.14rem' : '2.57rem')};
+  height: ${(props) => (props.size === 'medium' ? '3rem' : props.size === 'large' ? '3.14rem' : '2.67rem')};
+  line-height: ${(props) => (props.size === 'medium' ? '3rem' : props.size === 'large' ? '3.14rem' : '2.67rem')};
   background-color: ${(props) =>
     props.buttonTheme === 'primary' ? props.theme.colors.main : props.theme.colors.gray40};
   border-radius: ${(props) => props.theme.borderRadius};
