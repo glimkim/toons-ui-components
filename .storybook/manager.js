7b2fc3a43562419e206b05a8bf9d392761c0da2a
glimkim <limkimdev@gmail.com>
2022-08-05 12:41:11 +0900

    feat: change favicon and laucher icons

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,12 @@
 import { addons } from '@storybook/addons';
 import toonsTheme from './customStorybookTheme/ToonsTheme';
+import favicon from '../public/favicon.png';
 
 addons.setConfig({
   theme: toonsTheme,
 });
+
+const link = document.createElement('link');
+link.setAttribute('rel', 'shortcut icon');
+link.setAttribute('href', favicon);
+document.head.appendChild(link);
