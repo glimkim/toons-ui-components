5352386fdd106d6d491f9a1acd010e402785a2e4
glimkim <limkimdev@gmail.com>
2022-08-17 14:59:02 +0900

    feat: add div style on decorator to set components in the center

--- .storybook/decorators/CustomComponentWrapper.js
+++ .storybook/decorators/CustomComponentWrapper.js
@@ -3,10 +3,18 @@ import { GlobalStyles } from '../../src/styles/globalStyle.tsx';
 
 function CustomComponentWrapper(storyFn) {
   return (
-    <>
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <GlobalStyles />
       {storyFn()}
-    </>
+    </div>
   );
 }
 
