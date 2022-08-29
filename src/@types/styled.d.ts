12d74893162f33d67d363bc21a3c079e2d1b3de4
glimkim <limkimdev@gmail.com>
2022-08-29 13:28:04 +0900

    chore: remove unnecessary type definition

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,11 +1,6 @@
 import { Theme } from './../../src/themes/index';
 import 'styled-components';
 
-interface IPalette {
-  main: string;
-  contrast: string;
-}
-
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends Theme {}
